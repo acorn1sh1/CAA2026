192566843d05a86342ffdec7e7e7829b4c567c05
acorn1sh1 <120431723+acorn1sh1@users.noreply.github.com>
2026-04-19 10:30:26 -0500

    Add files via upload

---
+++ data/schedule.js
@@ -0,0 +1,90 @@
+// Edit this object between games/practices. Reload page to see changes.
+window.SCHEDULE = {
+  "events": [
+    {
+      "type": "practice",
+      "date": "2026-04-21T18:00",
+      "location": "Field 4",
+      "notes": "",
+      "practiceId": "p-2026-04-21"
+    },
+    {
+      "type": "practice",
+      "date": "2026-04-25T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-04-28T18:00",
+      "location": "Field 4",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-05-02T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-05-05T18:00",
+      "location": "Field 4",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-05-09T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-05-12T18:00",
+      "location": "Field 4",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-05-16T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-05-23T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-05-30T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-06-06T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-06-13T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-06-20T08:00",
+      "location": "Field 9",
+      "notes": ""
+    },
+    {
+      "type": "practice",
+      "date": "2026-06-27T08:00",
+      "location": "Field 9",
+      "notes": ""
+    }
+  ]
+};
