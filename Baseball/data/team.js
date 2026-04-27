a23cd9600fabefb7af6bf547a611b0b89beb9e34
acorn1sh1 <120431723+acorn1sh1@users.noreply.github.com>
2026-04-27 13:46:00 -0500

    Add files via upload

---
+++ Baseball/data/team.js
@@ -0,0 +1,32 @@
+window.TEAM = {
+  "team": {
+    "name": "Columbia TBD",
+    "season": "Spring 2026",
+    "league": "9U Kid Pitch / Coach Pitch",
+    "rules": {
+      "pitchFormat": "Kid pitch innings 1-2, coach pitch innings 3+",
+      "runCap": "5 runs per inning",
+      "innings": 7
+    },
+    "coach": {
+      "name": "Coach Andrew",
+      "contact": "andrewjcornish@gmail.com"
+    }
+  },
+  "roster": [
+    { "num": "", "name": "Greyson Cerminn",  "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Jack Cornish",     "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Albert Doerr",     "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Forrest Doerr",    "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Jude Durako",      "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Benton Herrmann",  "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Luca Lopez",       "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Michael Schirmer", "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Levi Schueler",    "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Nolan Schueler",   "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Jackson Segura",   "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Charlie Wachtel",  "pos": [], "bats": "", "throws": "" },
+    { "num": "", "name": "Parker White",     "pos": [], "bats": "", "throws": "" }
+  ]
+}
+;
