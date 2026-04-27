41250ee0a5be43e21b0157d07ff775c65929cda3
acorn1sh1 <120431723+acorn1sh1@users.noreply.github.com>
2026-04-27 13:49:43 -0500

    Add files via upload

---
+++ data/games.js
@@ -0,0 +1,281 @@
+window.GAMES = {
+  "games": [
+    {
+      "id": "g-2026-05-12",
+      "date": "2026-05-12T18:00",
+      "opponent": "Steck",
+      "home": false,
+      "location": "Columbia American Legion Field 4",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "First game of the season",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-05-14",
+      "date": "2026-05-14T18:00",
+      "opponent": "Search",
+      "home": false,
+      "location": "Columbia American Legion Field 4",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-05-19",
+      "date": "2026-05-19T18:00",
+      "opponent": "Duke",
+      "home": false,
+      "location": "Columbia American Legion Field 2",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-05-21",
+      "date": "2026-05-21T18:00",
+      "opponent": "Search",
+      "home": true,
+      "location": "Columbia American Legion Field 4",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-05-26",
+      "date": "2026-05-26T18:00",
+      "opponent": "Steck",
+      "home": true,
+      "location": "Columbia American Legion Field 4",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-05-28",
+      "date": "2026-05-28T18:00",
+      "opponent": "Duke",
+      "home": false,
+      "location": "Columbia American Legion Field 4",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-06-02",
+      "date": "2026-06-02T18:00",
+      "opponent": "Smithton 2",
+      "home": false,
+      "location": "Smithton Community Park Field 3",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-06-04",
+      "date": "2026-06-04T18:00",
+      "opponent": "Dupo",
+      "home": false,
+      "location": "Dupo Quarterback Club Field 1",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-06-09",
+      "date": "2026-06-09T18:00",
+      "opponent": "Valmeyer",
+      "home": true,
+      "location": "Columbia American Legion Field 4",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-06-11",
+      "date": "2026-06-11T18:00",
+      "opponent": "Smithton 1",
+      "home": true,
+      "location": "Columbia American Legion Field 2",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-06-16",
+      "date": "2026-06-16T18:00",
+      "opponent": "Red Bud",
+      "home": true,
+      "location": "Columbia American Legion Field 4",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    },
+    {
+      "id": "g-2026-06-18",
+      "date": "2026-06-18T18:00",
+      "opponent": "New Athens",
+      "home": true,
+      "location": "Columbia American Legion Field 2",
+      "teamRuns": 0,
+      "oppRuns": 0,
+      "result": "",
+      "notes": "",
+      "lineup": {
+        "battingOrder": [],
+        "defense": {
+          "1": {},
+          "2": {},
+          "3": {},
+          "4": {},
+          "5": {},
+          "6": {},
+          "7": {}
+        }
+      }
+    }
+  ]
+}
+;
